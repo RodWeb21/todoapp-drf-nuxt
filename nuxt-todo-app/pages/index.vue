7e112ca6d7fb4e4ab8c81386f75864032de2856a
Rodrigo <rodweb21@gmail.com>
2025-03-20 12:16:45 -0500

    Implementar el data fetching en Nuxt

---
+++ nuxt-todo-app/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+const { data } = useFetch('http://localhost:8000/api/todos/');
+
+</script>
+
+<template>
+  <div>
+    <h1>Lista de Tareas</h1>
+    <p>{{ data }}</p>
+  </div>
+</template>
