108858fcae3a98058264c1e54129defe271e957c
Rodrigo <rodweb21@gmail.com>
2025-03-20 16:56:35 -0500

    Implementar la función updateTodo

--- nuxt-todo-app/pages/index.vue
+++ nuxt-todo-app/pages/index.vue
@@ -1,11 +1,27 @@
 <script setup>
-const { data } = useFetch('http://localhost:8000/api/todos/');
+const { data: todos } = await useFetch('http://localhost:8000/api/todos/');
 
+const updateTodo = async (todo) => {
+  try {
+    await $fetch(`http://localhost:8000/api/todos/${todo.id}/`, {
+      method: 'PUT',
+      body: JSON.stringify({ title: todo.title, completed: todo.completed }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+  } catch (error) {
+    console.error('Error al actualizar tarea:', error);
+  }
+};
 </script>
 
 <template>
   <div>
     <h1>Lista de Tareas</h1>
-    <p>{{ data }}</p>
+    <p>{{ todos }}</p>
+    <div v-for="todo in todos">
+      <input type="checkbox" v-model="todo.completed" @change="updateTodo(todo)" />
+      <span>{{ todo.title }}</span>
+      <button @click="deleteTodo(todo.id)">Eliminar</button>
+    </div>
   </div>
 </template>
